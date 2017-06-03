4a1861c3dfa9f4481c6a0617e6f7db1a1185c574
Joseph Angeja <jmangeja@sensity.com>
2017-06-03 14:07:37 -0700

    All of sesions, get, post of Persons

--- Routes/CnnPool.js
+++ Routes/CnnPool.js
@@ -1,33 +1,35 @@
 var MongoClient = require('mongodb').MongoClient;
-// MongoClient = mysql
-var mongoURI = 'mongodb://localhost/MealPrep'
+var mongoURI = 'mongodb://localhost:27017/MealPrep';
 
 var CnnPool = function() {
    var poolCfg = require('./connection.json');
 
    poolCfg.connectionLimit = CnnPool.PoolSize;
-
    MongoClient.connect(mongoURI, {
      poolSize: CnnPool.PoolSize
 
    },function(err, db) {
-       if (!err)
-         CnnPool.mongodb = db;
+      if (!err) {
+         this.mongodb = db;
+      }
+      else {
+         console.log(JSON.stringify(err));
+      }
    }
    );
 };
 
 CnnPool.PoolSize = 4;
 
+
 // Router function for use in auto-creating CnnPool for a request
 CnnPool.router = function(req, res, next) {
    console.log("Getting connection");
-   req.cnn = CnnPool.singleton.mongodb
+   req.cnn = this.mongodb;
    console.log("Connection Acquired");
    next();
 };
 
 // The one (and probably only) CnnPool object needed for the app
 CnnPool.singleton = new CnnPool();
-
 module.exports = CnnPool;
