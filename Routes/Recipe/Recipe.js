a875f226e092ed7276b4584b7e34e6fec5e40cde
asnewman <ashznewman@gmail.com>
2017-06-03 14:16:27 -0700

    Adding recipes

---
+++ Routes/Recipe/Recipe.js
@@ -0,0 +1,84 @@
+var Express = require('express');
+var Tags = require('../Validator.js').Tags;
+var router = Express.Router({caseSensitive: true});
+var async = require('async');
+
+router.baseURL = '/Rcp';
+
+router.get('/', function(req, res) {
+   var vld = req.validator;
+
+   async.waterfall([
+   function(cb) {
+      if(vld.check(req.session), Tags.noPermission, null, cb) {
+         req.cnn.Recipe.find();
+      }
+   },
+   function(rcps, cb) {
+      res.status(200).json(rcps);
+      cb();
+   }],
+   function(err) {
+      // maybe need to release?
+   });
+});
+
+router.post('/', function(req, res) {
+   var vld = validator;
+
+   async.waterfall([
+   function(cb) {
+      if(vld.check(req.session), Tags.noPermission, null, cb &&
+       vld.hasFields(req.body, ["recipeId, date"], cb)) {
+         req.cnn.Recipe.insertOne({recipeId: req.body.recipeId, 
+          date: req.body.date, ownerId: req.session.id});
+      }
+   }],
+   function(err) {
+      if (!err) {
+         res.status(200).end();
+      }
+   });
+});
+
+router.delete('/:id', function(req, res) {
+   var vld = validator;
+
+   async.waterfall([
+   function(cb) {
+      if (vld.check(req.session), Tags.noPermission, null, cb) {
+         req.cnn.Recipe.find({id: req.params.id});
+      }
+   },
+   function(response, cb) {
+      if (vld.check(response.id === req.session.id), 
+       Tags.noPermission, null, cb) {
+         req.cnn.Recipe.deleteMany({id: req.params.id});
+      }
+   }],
+   function(err) {
+      if (!err) {
+         res.status(200).end();
+      }
+   });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
