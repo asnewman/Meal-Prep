7692c1484f666c05c7fd0220fc365733b9686b34
Andrew Kim <andrew.kym7@gmail.com>
2017-06-03 10:36:56 -0700

    added main.js and package.json

---
+++ main.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+var bodyParser = require('body-parser');
+var Session = require('./Routes/Session.js');
+var Validator = require('./Routes/Validator.js');
+var CnnPool = require('./Routes/CnnPool.js');
+
+var async = require('async');
+
+var app = express();
+
+// Static paths to be served like index.html and all client side js
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse all request bodies using JSON
+app.use(bodyParser.json());
+
+// Attach cookies to req as req.cookies.<cookieName>
+app.use(cookieParser());
+
+// Set up Session on req if available
+app.use(Session.router);
+
+// Check general login.  If OK, add Validator to |req| and continue processing,
+// otherwise respond immediately with 401 and noLogin error tag.
+app.use(function(req, res, next) {
+   console.log(req.path);
+   if (req.session || (req.method === 'POST' &&
+    (req.path === '/Prss' || req.path === '/Ssns'))) {
+      req.validator = new Validator(req, res);
+      next();
+   }
+
+   else
+      res.status(401).end();
+});
+
+// Add DB connection, with smart chkQry method, to |req|
+app.use(CnnPool.router);
+
+// Handler of last resort. Send a 500 response.
+app.use(function(req, res, next) {
+   res.status(404).end();
+   res.cnn.release();
+});
+
+// Parse out command line arguments for a -p port flag
+var port = 8080; // Default port
+process.argv.forEach(function (val, index, array) {
+   if (val === "-p")
+      port = process.argv[index + 1];
+});
+
+app.listen(port, function () {
+   console.log('App Listening on port ' + port);
+});
