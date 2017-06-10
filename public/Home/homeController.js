d99b9e5adf44325baf2ff1369251e4cb75aac33e
Andrew Kim <andrew.kym7@gmail.com>
2017-06-10 11:40:33 -0700

    Added a check for session time

--- public/Home/homeController.js
+++ public/Home/homeController.js
@@ -1,13 +1,32 @@
-app.controller('homeController', ['$scope', 'login', '$http',
- function($scope, login, $http) {
+app.controller('homeController', ['$scope', 'login', '$http', '$state',
+ function($scope, login, $http, $state) {
    $scope.loadPage = function() {
       var cookie = login.getCookie();
       var user = login.getCookieData();
-      // var allRecipes = $http.get('/Proxy/search?key=6c623c76c61436feae669486ad7aabc1');
+      var loginDate, nowDate;
 
-      // login.clearCookieData();
-      // console.log(allRecipes);
-
+      // If there is a cookie stored, check if the time has expired
+      // If time is over two hours then clear the cookie
+      if (cookie) {
+         (function() {
+            return $http.get("Ssns/" + cookie)
+             .then(function(response) {
+                return response.data.loginTime;
+             })
+             .then (function(loginTime) {
+                loginDate = new Date(loginTime);
+                nowDate = new Date();
+                // if check the time difference and clear cookie info if needed
+                if ((nowDate.getTime() - loginDate.getTime()) >
+                 (2 * 60 * 60 * 1000)) {
+                    login.clearCookieData();
+                    $state.go('home');
+                    window.location.reload();
+                }
+             })
+         })();
+      }
+      
       if (cookie && user)
          $scope.user = JSON.parse(user);
       }
