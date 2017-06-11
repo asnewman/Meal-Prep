ad43c258d9a1f542a28c67c63e35755d2abcc26c
Andrew Kim <andrew.kym7@gmail.com>
2017-06-11 10:16:36 -0700

    rebase

--- public/Home/homeController.js
+++ public/Home/homeController.js
@@ -7,25 +7,25 @@ app.controller('homeController', ['$scope', 'login', '$http', '$state',
 
       // If there is a cookie stored, check if the time has expired
       // If time is over two hours then clear the cookie
-      if (cookie) {
-         (function() {
-            return $http.get("Ssns/" + cookie)
-             .then(function(response) {
-                return response.data.loginTime;
-             })
-             .then (function(loginTime) {
-                loginDate = new Date(loginTime);
-                nowDate = new Date();
-                // if check the time difference and clear cookie info if needed
-                if ((nowDate.getTime() - loginDate.getTime()) >
-                 (2 * 60 * 60 * 1000)) {
-                    login.clearCookieData();
-                    $state.go('home');
-                    window.location.reload();
-                }
-             })
-         })();
-      }
+      // if (cookie) {
+      //    (function() {
+      //       return $http.get("Ssns/" + cookie)
+      //        .then(function(response) {
+      //           return response.data.loginTime;
+      //        })
+      //        .then (function(loginTime) {
+      //           loginDate = new Date(loginTime);
+      //           nowDate = new Date();
+      //           // if check the time difference and clear cookie info if needed
+      //           if ((nowDate.getTime() - loginDate.getTime()) >
+      //            (2 * 60 * 60 * 1000)) {
+      //               login.clearCookieData();
+      //               $state.go('home');
+      //               window.location.reload();
+      //           }
+      //        })
+      //    })();
+      // }
 
       // if ($scope.user) {
       //    (function() {
@@ -39,7 +39,7 @@ app.controller('homeController', ['$scope', 'login', '$http', '$state',
       // }
 
       // force Clear cookies
-      // login.clearCookieData();
+      login.clearCookieData();
 
       if (cookie && user)
          $scope.user = JSON.parse(user);
