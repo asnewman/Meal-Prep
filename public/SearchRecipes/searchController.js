8c12b6aeec98e4a026d7fa23184dcc3805d4a0c8
Andrew Kim <andrew.kym7@gmail.com>
2017-06-12 22:31:46 -0700

    added checkbox functionality

--- public/SearchRecipes/searchController.js
+++ public/SearchRecipes/searchController.js
@@ -1,8 +1,31 @@
 app.controller('searchController', ['$scope', 'login', '$http', '$state', 'ingr', '$mdDialog',
  function($scope, login, $http, $state, ingr, $mdDialog) {
     $scope.ingr = ingr;
+    $scope.selected = [];
+
+    $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+          list.splice(idx, 1);
+        }
+        else {
+          list.push(item);
+        }
+    };
+
+    $scope.exists = function (item, list) {
+      return list.indexOf(item) > -1;
+    };
 
     $scope.newRecipe = function() {
+      var searchString = "";
+      $scope.selected.forEach(function(item, idx, list) {
+         if (idx === list.length - 1)
+            searchString += item.name;
+         else
+            searchString += (item.name + ",");
+      });
+
       $mdDialog.show({
          controller: DialogController,
          templateUrl: '/SearchRecipes/newRecipe.template.html',
@@ -10,7 +33,7 @@ app.controller('searchController', ['$scope', 'login', '$http', '$state', 'ingr'
          resolve: {
             rcps: ['$q', '$http', '$rootScope',
              function($q, $http, $rootScope) {
-                return $http.get("/Proxy/search?key=6c623c76c61436feae669486ad7aabc1&sort=t")
+                return $http.get("/Proxy/search?key=6c623c76c61436feae669486ad7aabc1&q=" + searchString)
                  .then(function(response){
                     $rootScope.allRecipes = response.data.recipes;
                     return response.data.recipes;
